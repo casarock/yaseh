32e8f6b37c109bdcc2ed02f20549eb4726079418
Carsten Sandtner <mail@casa-rock.de>
2014-01-11 21:04:25 +0100

    added an optional conetxt when invoking a callback.

--- yaseh.js
+++ yaseh.js
@@ -3,7 +3,6 @@
     var queue = {};
 
     yaseh.register = function(identifier, callback) {
-
         if (typeof queue[identifier] === 'undefined') {
             queue[identifier] = [callback];
         }
@@ -12,16 +11,14 @@
         }
 
         return [identifier, callback];
-
     };
 
     yaseh.unregister = function(handle) {
-
         var identifier = handle[0],
             callback = handle[1],
             subscribed = queue[identifier] || [];
 
-        for(var i = 0, len = subscribed.length; i<len; i++) {
+        for (var i = 0, len = subscribed.length; i<len; i++) {
 
             if (subscribed[i] === callback) {
                 subscribed.splice(i, 1);
@@ -35,21 +32,19 @@
         else {
             queue[identifier] = subscribed;
         }
-
     };
 
-    yaseh.trigger = function(identifier, arguments) {
-
+    yaseh.trigger = function(identifier, arguments, context) {
         arguments = arguments || [];
+        context = context || yaseh;
 
         var cbs = queue[identifier] || [],
             len = cbs.length || 0;
 
 
         for(var idx = 0; idx < len; idx++) {
-            cbs[idx].apply(yaseh, arguments);
+            cbs[idx].apply(context, arguments);
         }
-
     };
 
 })(this);
